8aecbbe51b88ff3e7d6e89cf57eb9640c0495505
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-03-02 15:05:44 +0530

    Update PalindromeTest.java

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -14,9 +14,48 @@ public class PalindromeTest{
 public void checkstr()
 {
     Palindrome palindromeTester = new Palindrome();
-    assertTrue(palindromeTester.isWordPresent(" hi this is abhishek","hello"));
+    assertTrue(palindromeTester.isWordPresent(" hello world","hello"));
 	
-	
-
 }
+	@Test
+    public void testWordInString() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello"));
+    }
+
+    @Test
+    public void testWordNotInString() {
+        String sentence = "hello world";
+        assertFalse(sentence.contains("cat"));
+    }
+
+    @Test
+    public void testWordInEmptyString() {
+        String sentence = "";
+        assertFalse(sentence.contains("hello"));
+    }
+
+    @Test
+    public void testNullString() {
+        String sentence = null;
+        assertFalse(sentence.contains("hello"));
+    }
+    
+    @Test
+    public void testWordInStringCaseSensitive() {
+        String sentence = "hello world";
+        assertFalse(sentence.contains("Hello"));
+    }
+
+    @Test
+    public void testWordInStringCaseInsensitive() {
+        String sentence = "hello world";
+        assertTrue(sentence.toLowerCase().contains("hello"));
+    }
+    
+    @Test
+    public void testWordWithSpecialCharacters() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello!"));
+    }
 }
